6aa33c158701c414eeef1863e2546999e00b7904
Andrey Kutsenko <kutsenko@gmail.com>
2017-11-29 17:20:46 -0500

    Задание №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,11 +4,16 @@ import java.util.Random;
 
 public class Point {
 
-  public static void about() {
+  int[] p1 = new int[2];
+  int[] p2 = new int[2];
+  Random random = new Random();
 
-    int[] p1 = new int[2];
-    int[] p2 = new int[2];
-    Random random = new Random();
+//  public Point(int[] p1, int[] p2) {
+//    this.p1 = p1;
+//    this.p2 = p2;
+//  }
+
+  public double distance() {
 
     //устанавливаем координаты точки №1
     p1[0] = random.nextInt(10);
@@ -18,13 +23,6 @@ public class Point {
     p2[0] = random.nextInt(10);
     p2[1] = random.nextInt(10);
 
-    System.out.println("Координаты точки №1 (x1,y1): (" + p1[0] + "," + p1[1] + ")");
-    System.out.println("Координаты точки №2 (x2,y2): (" + p2[0] + "," + p2[1] + ")");
-    System.out.println("Расстояние между точками: " + distance(p1, p2));
-
-  }
-
-  public static double distance(int[] p1, int[] p2) {
     return Math.sqrt((p2[0] - p1[0]) * (p2[0] - p1[0]) + (p2[1] - p1[1]) * (p2[1] - p1[1]));
   }
 
