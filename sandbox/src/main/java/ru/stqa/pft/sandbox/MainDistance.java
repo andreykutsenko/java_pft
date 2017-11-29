3b6955e3f3017dc3c2845955cdc256f1d201d608
Andrey Kutsenko <kutsenko@gmail.com>
2017-11-29 16:25:20 -0500

    Запуск функции вычисления расстояния между точками

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MainDistance.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+public class MainDistance {
+
+  public static void main(String[] args) {
+
+    Point p = new Point();
+    p.about();
+  }
+
+}
+
+
+
+
+
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.Random;
+
+public class Point {
+
+  public static void about() {
+
+    int[] p1 = new int[2];
+    int[] p2 = new int[2];
+    Random random = new Random();
+
+    //устанавливаем координаты точки №1
+    p1[0] = random.nextInt(10);
+    p1[1] = random.nextInt(10);
+
+    //устанавливаем координаты точки №2
+    p2[0] = random.nextInt(10);
+    p2[1] = random.nextInt(10);
+
+    System.out.println("Координаты точки №1 (x1,y1): (" + p1[0] + "," + p1[1] + ")");
+    System.out.println("Координаты точки №2 (x2,y2): (" + p2[0] + "," + p2[1] + ")");
+    System.out.println("Расстояние между точками: " + distance(p1, p2));
+
+  }
+
+  public static double distance(int[] p1, int[] p2) {
+    return Math.sqrt((p2[0] - p1[0]) * (p2[0] - p1[0]) + (p2[1] - p1[1]) * (p2[1] - p1[1]));
+  }
+
+}
