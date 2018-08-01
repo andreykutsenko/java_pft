430a4719b1a64b1cf05c6ccc218e63203fac7434
Andrey Kutsenko <kutsenko@gmail.com>
2018-07-31 22:06:41 -0400

    Лекция 5.8. Хеширование и предварительные проверки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -59,6 +59,10 @@ public class GroupHelper extends HelperBase {
     returnToGroupPage();
   }
 
+  public int count() {
+    return wd.findElements(By.name("selected[]")).size();
+  }
+
   private Groups groupCache = null;
 
   public Groups all() {
