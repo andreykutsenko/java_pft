891df9e3fa817c65e641938c85ba5fe6cf89959d
Andrey Kutsenko <kutsenko@gmail.com>
2018-09-17 01:58:24 -0400

    Лекция 9.6. REST: взаимодействие с GitHub

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("9554b277bab4dc5a70d8255dc8ecffc9c2e943a1");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("andreykutsenko", "java_pft")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
